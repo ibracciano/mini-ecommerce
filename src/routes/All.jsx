556c8b4b156fcb4d0de33fdca6e69bf324a2e710
ouattara <ouattaraibrahimyacouba@gmail.com>
2024-05-07 14:49:45 +0000

    version6

--- src/routes/All.jsx
+++ src/routes/All.jsx
@@ -10,14 +10,16 @@ const All = () => {
         {data.map((item) => (
           <li
             key={item.id}
-            className="p-5 bg-white  border rounded-md shadow-lg md:h-[230px]"
+            className="p-5 bg-white border rounded-md shadow-lg md:h-[260px]"
           >
-            <div>
+            <div className="pb-10">
               <img
                 src={item.image}
                 alt={item.name}
-                className="md:w-[50%] md:h-[50%] mx-auto"
+                className="md:w-[30%] md:h-[30%] mx-auto"
               />
+              <h3 className="mt-3 text-[10px] text-center font-semibold">{item.title}</h3>
+              <p className="mt-3 text-[12px] text-center font-bold">${item.price}</p>
               <button className="px-1 py-2 md:py-1 w-[50%] md:w-[70%] md:ml-[15%] ml-[25%] mt-3 text-white bg-purple-600 rounded-md">
                 <Link
                   to={`/product/${item.id}`}
